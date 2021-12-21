22a09bd1281e795e09c130f6281a3ad15b4b11ba
jeet-lanjewar <91592248+jeet-lanjewar@users.noreply.github.com>
2021-12-21 21:36:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var heading = "MY first console project";
+console.log(heading);
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+var b = 5;
+var add =a+b;
+console.log(add);
+console.log(b);
+var d = 50;
+console.log(d);
+var c ="2";
+console.log(c);
